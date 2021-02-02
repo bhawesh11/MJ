b6f3bfc1f545a008cb7f34c98edca4c41cb7bcd5
aro99999 <69912083+aro99999@users.noreply.github.com>
2021-02-02 16:04:52 +0530

    Pushing changes

--- src/test/java/SharedTC/SharedTestCases_VehicleDetails1of2.java
+++ src/test/java/SharedTC/SharedTestCases_VehicleDetails1of2.java
@@ -60,10 +60,10 @@ public class SharedTestCases_VehicleDetails1of2 {
         test.setPage(VehicleDetails2of2.class);
         VehicleDetails2of2 vehicleDetails2of2 = (VehicleDetails2of2) PageFactory.initElements(test.driver, test.getPage());
         test.webFunctions().click(test,vehicleDetails2of2.dropDownYearBought);
-        test.webFunctions().click(test,vehicleDetails2of2.dropDownYearBought_Select, test.getTestData("Vehicle.V1.YearBought"));
+        test.webFunctions().click(test,vehicleDetails2of2.dropDownYearBought_Select, test.getTestData("Vehicle.V"+count+ ".YearBought"));
         test.webFunctions().click(test,vehicleDetails2of2.dropDownMonthBought);
-        test.webFunctions().click(test,vehicleDetails2of2.dropDownMonthBought_Select, test.getTestData("Vehicle.V1.MonthBought"));
-        test.webFunctions().click(test,vehicleDetails2of2.btn_OwnedByYou, test.getTestData("Vehicle.V1.Ownedbyyou"));
+        test.webFunctions().click(test,vehicleDetails2of2.dropDownMonthBought_Select, test.getTestData("Vehicle.V"+count+".MonthBought"));
+        test.webFunctions().click(test,vehicleDetails2of2.btn_OwnedByYou, test.getTestData("Vehicle.V"+count+".Ownedbyyou"));
         test.webFunctions().click(test,vehicleDetails2of2.checkBox_HandsFree);
         test.webFunctions().click(test,vehicleDetails2of2.checkBox_CrashAvoidance);
         test.webFunctions().click(test,vehicleDetails2of2.btn_Next);
