8b6449ac19d2f56a26e7fa5ef34593315e1d131b
akanmol31 <44868596+akanmol31@users.noreply.github.com>
2021-02-05 21:43:43 +0530

    Final Test Scripts

--- src/test/java/SharedTC/SharedTestCases_PolicyHolderDetails1of2.java
+++ src/test/java/SharedTC/SharedTestCases_PolicyHolderDetails1of2.java
@@ -6,6 +6,8 @@ import resources.TestData;
 import resources.Testing;
 import resources.WebFunctions;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SharedTestCases_PolicyHolderDetails1of2 {
@@ -14,14 +16,74 @@ public class SharedTestCases_PolicyHolderDetails1of2 {
     public void policyHolderDetails1of2(Testing test) {
         test.setPage(PolicyholderDetails1of2.class);
         PolicyholderDetails1of2 policyholderDetails1of2 = (PolicyholderDetails1of2) PageFactory.initElements(test.driver, test.getPage());
+        test.webFunctions().staticWait(3000);
         test.webFunctions().click(test,policyholderDetails1of2.btn_Gender, test.getTestData("PolicyholderDetails.Gender"));
         test.webFunctions().click(test,policyholderDetails1of2.btn_MaritalStatus,test.getTestData("PolicyholderDetails.MaritalStatus"));
-        test.webFunctions().click(test,policyholderDetails1of2.btn_Education,test.getTestData("PolicyholderDetails.Education"));
-        test.webFunctions().click(test,policyholderDetails1of2.btn_CurrentlyEmployed,test.getTestData("PolicyholderDetails.Employment"));
+        try {
+        if(policyholderDetails1of2.text_EducationCompleted.isDisplayed()==true)
+        {
+        	test.webFunctions().click(test,policyholderDetails1of2.btn_Education,test.getTestData("PolicyholderDetails.Education"));
+            test.webFunctions().click(test,policyholderDetails1of2.btn_CurrentlyEmployed,test.getTestData("PolicyholderDetails.Employment"));
+        }}
+        catch (Exception e) {
+			// TODO: handle exception
+		}
+        wfhDriveToWork(test);
         test.webFunctions().click(test,policyholderDetails1of2.btn_Continue);
         test.getLogger().info("Policy holder Details 1of2 page: Success!");
     }
 
     // -------------------------------------------------------------------------------------------
-
+    
+   /* public void currentlyEmployed(Testing test) {
+    	 test.setPage(PolicyholderDetails1of2.class);
+         PolicyholderDetails1of2 policyholderDetails1of2 = (PolicyholderDetails1of2) PageFactory.initElements(test.driver, test.getPage());
+         test.webFunctions().click(test,policyholderDetails1of2.btn_CurrentlyEmployed,test.getTestData("PolicyholderDetails.Employment"));
+         test.webFunctions().staticWait(3000);
+         try {
+        	 if(policyholderDetails1of2.radiobutton_Days.isDisplayed()==true)
+        	 {
+        		 test.webFunctions().click(test,policyholderDetails1of2.btn_DaysYouDrive,test.getTestData("PolicyholderDetails.DaysYouDriveToWork"));
+        		 test.webFunctions().click(test,policyholderDetails1of2.btn_WFHOrAttendClasses,test.getTestData("PolicyholderDetails.WFH"));
+        		 
+        		 try {
+        		 if(policyholderDetails1of2.textbox_CurrentOccupation.isDisplayed()==true) 
+        		 {
+        			 test.webFunctions().staticWait(2000);
+        			 test.webFunctions().dropdownMelissaJS(test,policyholderDetails1of2.textbox_CurrentOccupation,test.getTestData("PolicyholderDetails.CurrentOccupation"));
+        		 }	
+        		 }catch (Exception e) {}
+  
+        		 try {
+        			 if(policyholderDetails1of2.btn_Army.isDisplayed()==true) {
+        				 System.out.println("Hi There");
+        				 test.webFunctions().click(test,policyholderDetails1of2.btn_MilitaryBranch_School,test.getTestData("PolicyholderDetails.MilitaryBranch/School"));
+            			 test.webFunctions().staticWait(2000);
+            			 test.webFunctions().click(test,policyholderDetails1of2.dropdown_militaryStatus);
+            			 test.webFunctions().staticWait(2000);
+            			 test.webFunctions().click(test,policyholderDetails1of2.dropdown_SelectRank,test.getTestData("PolicyholderDetails.Rank"));
+        			 }
+        		 }catch (Exception e) {}
+        	 }
+        		 
+         }catch (Exception e) {}
+         test.webFunctions().click(test,policyholderDetails1of2.btn_MilitaryBranch_School,test.getTestData("PolicyholderDetails.MilitaryBranch/School"));
+         
+         
+    } */
+    
+    //**Only For GA State**
+    public void wfhDriveToWork(Testing test)
+    {
+     test.setPage(PolicyholderDetails1of2.class);
+     PolicyholderDetails1of2 policyholderDetails1of2 = (PolicyholderDetails1of2) PageFactory.initElements(test.driver, test.getPage());
+    	try {
+       	 if(policyholderDetails1of2.radiobutton_Days.isDisplayed()==true)
+       	 {
+       		 test.webFunctions().click(test,policyholderDetails1of2.btn_DaysYouDrive,test.getTestData("PolicyholderDetails.DaysYouDriveToWork"));
+       		 test.webFunctions().click(test,policyholderDetails1of2.btn_WFHOrAttendClasses,test.getTestData("PolicyholderDetails.WFH"));
+       		 test.webFunctions().click(test,policyholderDetails1of2.btn_InSchool,test.getTestData("PolicyholderDetails.InSchool"));
+       	 }
+    }catch (Exception e) {}
+    }
 }
