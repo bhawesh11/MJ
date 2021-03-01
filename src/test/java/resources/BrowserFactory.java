5764be6a80c839e07482725e632f736af548d823
aro99999 <69912083+aro99999@users.noreply.github.com>
2021-03-01 13:51:08 +0530

    taking pull

--- src/test/java/resources/BrowserFactory.java
+++ src/test/java/resources/BrowserFactory.java
@@ -13,7 +13,7 @@ public class BrowserFactory {
 	// CONSTRUCTOR
 	BrowserFactory(String browser, String url) {
 
-		Path chromeDriverPath = Paths.get("C:\\WorkSpace\\Drivers\\Browser\\chromedriver_win32\\chromedriver.exe");
+		Path chromeDriverPath = Paths.get("D:\\Project\\chromedriver.exe");
 
 
 		System.setProperty("webdriver.chrome.driver",chromeDriverPath.toString());
