25ed50308da21c5855fba6ee9f1929cc75083841
dkumar0104 <dkumar0104@gmail.com>
2021-03-02 16:26:10 +0530

    Arranged the testcases and correct the TC no in script name for TC04
    Arranged the testcases and correct the TC no in script name for TC04

--- src/test/java/testApparent/TC_Regression.java
+++ src/test/java/testApparent/TC_Regression.java
@@ -56,12 +56,54 @@ public class TC_Regression {
 	// ==============================================================================================
 //	***_TEST_CASES_***
 	
+
+	  // TC001
+  @Test(enabled = true, priority = 5, description = "V3_D1_001_TX_ACH")
+  @Parameters("ENV")
+  public void TC001(String ENV) throws Throwable {
+
+
+
+      Testing test = new Testing(ENV, brandName, "V3_D1_001_TX_ACH");
+      try {
+
+          PostalCode.postalCode(test);
+          Family.family(test);
+          Children.children(test);
+          PolicyHolder.policyholder(test);
+          Address.address(test);
+          VehicleDetails1of2.vehicleDetails(test);
+          PolicyHolderDetails1of2.policyHolderDetails1of2(test);
+          PolicyHolderDetails2of2.policyHolderDetails2of2(test);
+          DriverList.driverList(test);
+          InsuranceHistory.insuranceHistory_Yes(test);
+          PolicyStatrDate.policyStartDate(test);
+          test.webFunctions().staticWait(8000);
+          Quote.quote(test);
+          ConfirmDriver.confirmDriver(test);
+          test.webFunctions().staticWait(4000);
+          ConfirmVehicle.confirmVehicle(test);
+          test.webFunctions().staticWait(8000);
+          Billing.billing_Apparent(test);
+          Billing.fillFutureACHDetails_Apparent(test);
+          GetInfo.GetPolicyNumber(test);
+          test.markPassed();
+          
+      } catch (Throwable e) {
+          test.markFailed(e.getMessage());
+          throw (e);
+      } finally {
+          //test.tearDown();
+      }
+  }// closing TC001 method
+	//-------------------------------------------------------------
+  
 	// TC004
-			@Test(enabled = true, priority = 5, description = "V1_D1_3_DUI/DWI")
+			@Test(enabled = true, priority = 5, description = "V1_D1_04_DUI/DWI")
 			@Parameters("ENV")
 			public void TC004(String ENV) throws Throwable {
 
-				Testing test = new Testing(ENV, brandName, "V1_D1_3_DUI/DWI");
+				Testing test = new Testing(ENV, brandName, "V1_D1_04_DUI/DWI");
 				try {
 
 					PostalCode.postalCode(test);
@@ -83,46 +125,6 @@ public class TC_Regression {
 			}// closing TC004 method
 // ==============================================================================================
 
-	  // TC001
-    @Test(enabled = true, priority = 5, description = "V3_D1_001_TX_ACH")
-    @Parameters("ENV")
-    public void TC001(String ENV) throws Throwable {
-
- 
-
-        Testing test = new Testing(ENV, brandName, "V3_D1_001_TX_ACH");
-        try {
-
-            PostalCode.postalCode(test);
-            Family.family(test);
-            Children.children(test);
-            PolicyHolder.policyholder(test);
-            Address.address(test);
-            VehicleDetails1of2.vehicleDetails(test);
-            PolicyHolderDetails1of2.policyHolderDetails1of2(test);
-            PolicyHolderDetails2of2.policyHolderDetails2of2(test);
-            DriverList.driverList(test);
-            InsuranceHistory.insuranceHistory_Yes(test);
-            PolicyStatrDate.policyStartDate(test);
-            test.webFunctions().staticWait(8000);
-            Quote.quote(test);
-            ConfirmDriver.confirmDriver(test);
-            test.webFunctions().staticWait(4000);
-            ConfirmVehicle.confirmVehicle(test);
-            test.webFunctions().staticWait(8000);
-            Billing.billing_Apparent(test);
-            Billing.fillFutureACHDetails_Apparent(test);
-            GetInfo.GetPolicyNumber(test);
-            test.markPassed();
-            
-        } catch (Throwable e) {
-            test.markFailed(e.getMessage());
-            throw (e);
-        } finally {
-            //test.tearDown();
-        }
-    }// closing TC001 method
-	
 	
 	// TC006
 	@Test(enabled = true, priority = 5, description = "V1_D2_06")
